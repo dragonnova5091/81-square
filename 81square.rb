0cb948a74824291264b7aae0cde2c0ef8c68ca27
Student <Student>
2016-11-18 08:43:00 -0800

    a

--- 81square.rb
+++ 81square.rb
@@ -1,9 +1,23 @@
 require 'gosu'
 
-class Game 
+class GameWindow < Gosu::Window
+	def initialize
+		super(800, 800)
+		self.caption = "81 Square"
+	end
+end
+
+class X
 	def initialize
 		
 	end
 end
 
-game = Game.new
+class O
+	def initialize
+		
+	end
+end
+
+game = GameWindow.new
+game.show
